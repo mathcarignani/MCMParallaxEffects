2de6c82d0d91918d145d891b9465ad5364bd0bb7
Mathias Carignani <mathcarignani@gmail.com>
2013-09-28 12:55:27 -0300

    Initial Commit

---
+++ MCMParallaxEffects/MCMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCMAppDelegate.h
+//  MCMParallaxEffects
+//
+//  Created by Mathias on 28/09/13.
+//  Copyright (c) 2013 shoock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MCMParallaxEffects/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MCMParallaxEffects
+//
+//  Created by Mathias on 28/09/13.
+//  Copyright (c) 2013 shoock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCMAppDelegate class]));
+    }
+}
